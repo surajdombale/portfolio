ebfea2c6653982521ba648c1709a8a8b64d02cb2
surajdombale <surajdombale@gmail.com>
2026-05-14 21:37:01 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
+
+  const isGithubPages = env.VITE_DEPLOY_TARGET === 'github';
+
   return {
-    base: '/portfolio/',
+    base: isGithubPages ? '/portfolio/' : '/',
+
     plugins: [react(), tailwindcss()],
     server: {
-      allowedHosts: ['.ngrok-free.app']
+      allowedHosts: ['.ngrok-free.app',
+        '.tiny.site',
+        'surajdombale.github.io'
+      ]
     },
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
